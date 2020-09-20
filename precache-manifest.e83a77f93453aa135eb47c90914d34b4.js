e04d588161c9786627b603fb32b544b41911fef5
kishibe-rohan <shahnawaz.21810589@viit.ac.in>
2020-09-20 20:56:01 +0530

    Updates

---
+++ precache-manifest.e83a77f93453aa135eb47c90914d34b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0669c67ce6ae5e0df29d",
+    "url": "/react-flix/static/js/runtime~main.1ec94c4c.js"
+  },
+  {
+    "revision": "b3042bab57d16a298831",
+    "url": "/react-flix/static/js/main.1ed81e94.chunk.js"
+  },
+  {
+    "revision": "bdd850158d335b3844e7",
+    "url": "/react-flix/static/js/2.be425d27.chunk.js"
+  },
+  {
+    "revision": "b3042bab57d16a298831",
+    "url": "/react-flix/static/css/main.4328fc71.chunk.css"
+  },
+  {
+    "revision": "e238620f92848514e99e1eae1bcfb1bb",
+    "url": "/react-flix/index.html"
+  }
+];
